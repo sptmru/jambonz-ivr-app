ca5ca36cf9d4c071039d453119f8cdf6a181c821
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-07 11:29:49 +0400

    add jambons API credentials to config

--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -9,4 +9,9 @@ export const config = {
     hostname: parsedConfig?.HTTP_HOSTNAME != null ? parsedConfig.HTTP_HOSTNAME : 'http://localhost',
     basePrefix: parsedConfig?.API_BASE_PREFIX != null ? parsedConfig.API_BASE_PREFIX : '/api/v1',
   },
+  jambonz: {
+    sid: parsedConfig?.JAMBONZ_ACCOUNT_SID != null ? Number(parsedConfig.JAMBONZ_ACCOUNT_SID) : 1,
+    apiKey: parsedConfig?.JAMBONZ_API_KEY != null ? parsedConfig.JAMBONZ_API_KEY : '1',
+    baseUrl: parsedConfig?.JAMBONZ_BASE_URL != null ? parsedConfig.JAMBONZ_BASE_URL : 'https://api.jambonz.org',
+  },
 };
