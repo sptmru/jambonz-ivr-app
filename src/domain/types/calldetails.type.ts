ffeacbd48f9ef339df330d5b1b603509a2248f54
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-07 10:57:09 +0400

    add type and definition for call details

---
+++ src/domain/types/calldetails.type.ts
@@ -0,0 +1,13 @@
+export type CallDetails = {
+  numberTo: string;
+  numberFrom: string;
+  wavUrlAnnounce: string;
+  wavUrlContinue: string;
+  wavUrlOptOut: string;
+  wavUrlVM: string;
+  transactionId: string;
+  carrierAddress: string;
+  destinationAddress: string;
+  digitContinue: string;
+  digitOptOut: string;
+};
