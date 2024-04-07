6f9103e9d04d2025db3ef066588e6b63168d831c
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-07 17:53:32 +0400

    add AMD callback payload type

---
+++ src/domain/types/amdresult.type.ts
@@ -0,0 +1,14 @@
+export type AmdResult = {
+  type: AmdResultEnum;
+  reason?: string;
+  hint?: string;
+  language?: string;
+};
+
+// eslint-disable-next-line no-shadow
+export enum AmdResultEnum {
+  HUMAN = 'amd_human_detected',
+  MACHINE = 'amd_machine_detected',
+  NO_SPEECH = 'amd_no_speech_detected',
+  DECISION_TIMEOUT = 'amd_decision_timeout',
+}
