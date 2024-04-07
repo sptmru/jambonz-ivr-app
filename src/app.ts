adad1b048a7b0484da81d66b1d49e75bb6c85f12
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-07 11:01:53 +0400

    add call details definition to Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import { CallDetailsDefinition } from './domain/definitions/calldetails.definition';
 import { Api } from './infrastructure/api/server';
 import { HealthRoute } from './routes/health/health.route';
 
 const api = new Api({
   plugins: [],
   routes: [HealthRoute],
-  definitions: [],
+  definitions: [CallDetailsDefinition],
 });
 
 api.listen();
