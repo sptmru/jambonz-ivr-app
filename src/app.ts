954df3f5b7bc79f703a354168b4fe973e697d1a4
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-07 13:54:57 +0400

    enable jambonz callback routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { CallDetailsDefinition } from './domain/definitions/calldetails.definition';
 import { Api } from './infrastructure/api/server';
+import { CallbacksRoute } from './routes/callback/callback.route';
 import { CallsRoute } from './routes/calls/calls.route';
 import { HealthRoute } from './routes/health/health.route';
 
 const api = new Api({
   plugins: [],
-  routes: [HealthRoute, CallsRoute],
+  routes: [HealthRoute, CallsRoute, CallbacksRoute],
   definitions: [CallDetailsDefinition],
 });
 
