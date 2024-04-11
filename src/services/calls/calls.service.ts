5eacf4702ba160c645c0e49dc07d391d365974ae
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-11 09:58:18 +0400

    do phone number validation when making a call to understand if we call to a phone number or to a SIP user

--- src/services/calls/calls.service.ts
+++ src/services/calls/calls.service.ts
@@ -2,14 +2,16 @@ import { CallDetails } from '../../domain/types/calldetails.type';
 import { config } from '../../infrastructure/config/config';
 import { RedisClient } from '../../infrastructure/redis/client';
 import { jambonz } from '../jambons/jambons-api-wrapper.service';
+import { PhoneNumberValidatorService } from './phonenumbervalidator.service';
 
 export class CallsService {
   static async createCall(callDetails: CallDetails): Promise<void> {
+    const phoneNumber = PhoneNumberValidatorService.validatePhoneNumber(callDetails.numberTo);
     const callId = await jambonz.calls.create({
       from: callDetails.numberFrom,
       to: {
-        type: 'user', // TODO: change type to "phone"
-        name: `${callDetails.numberTo}@${config.jambonz.sipRealm}`,
+        type: phoneNumber ? 'phone' : 'user',
+        name: phoneNumber ? phoneNumber.number : `${callDetails.numberTo}@${config.jambonz.sipRealm}`,
       },
       application_sid: config.jambonz.applicationSid,
       amd: { actionHook: `${config.jambonz.callbackBaseUrl}/api/v1/amd-callback` },
--- src/services/calls/phonenumbervalidator.service.ts
+++ src/services/calls/phonenumbervalidator.service.ts
@@ -1,7 +1,15 @@
 import parsePhoneNumber, { PhoneNumber } from 'libphonenumber-js';
+import { logger } from '../../misc/Logger';
 
 export class PhoneNumberValidatorService {
   static validatePhoneNumber(phoneNumber: string): PhoneNumber | undefined {
-    return parsePhoneNumber(phoneNumber);
+    logger.debug(`Validating phone number: ${phoneNumber}`);
+    const validatedNumber = parsePhoneNumber(phoneNumber);
+    if (validatedNumber && validatedNumber.isValid()) {
+      logger.debug(`Phone number ${validatedNumber.number} is a valid ${validatedNumber.country} number`);
+    } else {
+      logger.error(`Phone number ${phoneNumber} is not valid, considering that it is a SIP address`);
+    }
+    return validatedNumber;
   }
 }
