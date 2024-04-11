16c27e74c32d1744958fe3499828d8f11770eecc
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-11 10:42:37 +0400

    add Redis connection check to createCall API endpoint

--- src/controllers/calls/calls.controller.ts
+++ src/controllers/calls/calls.controller.ts
@@ -2,9 +2,14 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { CallDetails } from '../../domain/types/calldetails.type';
 import { logger } from '../../misc/Logger';
 import { CallsService } from '../../services/calls/calls.service';
+import { RedisClient } from '../../infrastructure/redis/client';
 
 export class CallsController {
   static async createCall(request: FastifyRequest<{ Body: CallDetails }>, reply: FastifyReply): Promise<FastifyReply> {
+    if (!RedisClient.getInstance().isConnected) {
+      logger.error('Redis is not connected');
+      return reply.code(500).send({ error: 'Internal server error' });
+    }
     const callDetails = request.body;
     try {
       await CallsService.createCall(callDetails);
