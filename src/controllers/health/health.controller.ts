e32ef6deb7b5ce6c994c671dc649a8a08f4dcbb2
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-12 22:20:30 +0400

    add Jambonz app ID check in the healthcheck

--- src/controllers/health/health.controller.ts
+++ src/controllers/health/health.controller.ts
@@ -1,13 +1,24 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { RedisClient } from '../../infrastructure/redis/client';
 import { MQClient } from '../../infrastructure/rabbitmq/client';
+import { isApplicationExists } from '../../services/jambons/jambons-api-wrapper.service';
 
 export class HealthController {
-  static healthcheck(_request: FastifyRequest, reply: FastifyReply): FastifyReply {
+  static async healthcheck(_request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> {
     const redisConnected = RedisClient.getInstance().isConnected;
-    const mqConsuming = MQClient.getInstance().isConnected;
-    return redisConnected && mqConsuming
-      ? reply.code(200).send({ message: 'API is alive!' })
-      : reply.code(500).send({ error: mqConsuming ? 'Redis connection lost' : 'RabbitMQ connection lost' });
+    const mqConnected = MQClient.getInstance().isConnected;
+    const jambonzAppExists = await isApplicationExists();
+    if (!(redisConnected && mqConnected && jambonzAppExists)) {
+      let errMessage: string;
+      if (!redisConnected) {
+        errMessage = 'Redis connection lost';
+      } else if (!mqConnected) {
+        errMessage = 'RabbitMQ connection lost';
+      } else {
+        errMessage = 'Jambonz application not found, check your Jambonz credentials';
+      }
+      return reply.code(500).send({ error: errMessage });
+    }
+    return reply.code(200).send({ message: 'API is alive!' });
   }
 }
--- src/services/jambons/jambons-api-wrapper.service.ts
+++ src/services/jambons/jambons-api-wrapper.service.ts
@@ -1,5 +1,16 @@
 import jambonzClient from '@jambonz/node-client';
 
 import { config } from '../../infrastructure/config/config';
+import axios from 'axios';
 
 export const jambonz = jambonzClient(config.jambonz.sid, config.jambonz.apiKey, { baseUrl: config.jambonz.baseUrl });
+export const isApplicationExists = async (): Promise<boolean> => {
+  try {
+    await axios.get(`${config.jambonz.baseUrl}/v1/Applications/${config.jambonz.applicationSid}`, {
+      headers: { Authorization: `Bearer ${config.jambonz.apiKey}` },
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
