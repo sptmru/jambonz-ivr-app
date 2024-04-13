0ce5f62ac503d86e057ed21f3210c46447fbba5b
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-13 11:54:58 +0400

    add Sentry transport to logger

--- src/misc/Logger.ts
+++ src/misc/Logger.ts
@@ -1,4 +1,5 @@
 import * as winston from 'winston';
+import Sentry from 'winston-transport-sentry-node';
 
 import { config } from '../infrastructure/config/config';
 
@@ -8,11 +9,18 @@ const timestampFormat = winston.format.combine(
   winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
 );
 
+const sentryOptions = {
+  sentry: {
+    dsn: config.sentry.dsn,
+  },
+  level: config.sentry.logLevel,
+};
+
 const logger = winston.createLogger({
   level: config.logLevel,
   format: timestampFormat,
   defaultMeta: {},
-  transports: [new winston.transports.Console()],
+  transports: [new winston.transports.Console(), new Sentry(sentryOptions)],
 });
 
 export { logger };
