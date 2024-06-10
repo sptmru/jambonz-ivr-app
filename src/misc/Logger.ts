158e5fe6f2b793f9ceaf48fe41620705730d1edb
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-10 13:08:08 +0400

    add a way to send logs to Loki to Logger class

--- src/misc/Logger.ts
+++ src/misc/Logger.ts
@@ -1,7 +1,12 @@
 import * as winston from 'winston';
 import Sentry from 'winston-transport-sentry-node';
+import LokiTransport from 'winston-loki';
 
 import { config } from '../infrastructure/config/config';
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
+import SentryTransport from 'winston-transport-sentry-node';
+
+type TransportUnion = LokiTransport | SentryTransport | FileTransportInstance | ConsoleTransportInstance;
 
 const consoleLogFormat = winston.format.combine(
   winston.format.colorize(),
@@ -21,16 +26,38 @@ const sentryOptions = {
   level: config.sentry.logLevel,
 };
 
+const lokiOptions = {
+  host: config.loki.host,
+  labels: config.loki.labels,
+  json: config.loki.json,
+  format: winston.format.json(),
+  interval: config.loki.interval,
+  timeout: config.loki.timeout,
+  onConnectionError: (err: Error): void => {
+    console.error(err);
+  },
+};
+
+const transportListWithFile: TransportUnion[] = [
+  new winston.transports.Console({ format: consoleLogFormat }),
+  new Sentry(sentryOptions),
+  new winston.transports.File({ filename: `${config.log.directory}/${config.log.file}`, format: fileLogFormat }),
+];
+
+const transportListWithoutFile: TransportUnion[] = [
+  new winston.transports.Console({ format: consoleLogFormat }) as ConsoleTransportInstance,
+  new Sentry(sentryOptions) as SentryTransport,
+];
+
+if (config.loki.enabled) {
+  transportListWithFile.push(new LokiTransport(lokiOptions));
+  transportListWithoutFile.push(new LokiTransport(lokiOptions));
+}
+
 const logger = winston.createLogger({
   level: config.log.level,
   defaultMeta: {},
-  transports: config.log.logToFile
-    ? [
-        new winston.transports.Console({ format: consoleLogFormat }),
-        new Sentry(sentryOptions),
-        new winston.transports.File({ filename: `${config.log.directory}/${config.log.file}`, format: fileLogFormat }),
-      ]
-    : [new winston.transports.Console({ format: consoleLogFormat }), new Sentry(sentryOptions)],
+  transports: config.log.logToFile ? transportListWithFile : transportListWithoutFile,
 });
 
 export { logger };
