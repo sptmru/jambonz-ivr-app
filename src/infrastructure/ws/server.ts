414886f281b3eaae7241969b6966c14d1477407d
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-22 23:04:10 +0400

    update WS server

--- src/infrastructure/ws/server.ts
+++ src/infrastructure/ws/server.ts
@@ -1,13 +1,25 @@
 import { createServer, Server } from 'http';
+import { createEndpoint, Client } from '@jambonz/node-client-ws';
 
 import { config } from '../config/config';
 import { logger } from '../../misc/Logger';
 
 export class WebsocketServer {
   public server: Server;
+  public initService: (params: { path: string }) => Client;
 
-  constructor() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(init: { routes: any }) {
+    this.routes(init.routes);
     this.server = createServer();
+    this.initService = createEndpoint({ server: this.server });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private routes(routes: { forEach: (arg0: (route: any) => void) => void }): void {
+    routes.forEach(route => {
+      void new route(this.initService);
+    });
   }
 
   public listen(): void {
