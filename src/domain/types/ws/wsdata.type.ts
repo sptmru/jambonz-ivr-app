97513f72f0ad6d062a9ab79cd891f972a4326bfe
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-23 19:23:39 +0400

    add WSData type

---
+++ src/domain/types/ws/wsdata.type.ts
@@ -0,0 +1,7 @@
+import { Client, Session } from '@jambonz/node-client-ws';
+// import { WsEvent } from './base/event.interface';
+
+export type WsData = {
+  client: Client;
+  session: Session;
+};
