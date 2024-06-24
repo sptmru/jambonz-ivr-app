97ec83edabef5af32a5c44cfc4b48758a561e9b5
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-24 06:03:31 +0400

    update WsData type

--- src/domain/types/ws/wsdata.type.ts
+++ src/domain/types/ws/wsdata.type.ts
@@ -2,6 +2,6 @@ import { Client, Session } from '@jambonz/node-client-ws';
 // import { WsEvent } from './base/event.interface';
 
 export type WsData = {
-  client: Client;
+  client?: Client;
   session: Session;
 };
