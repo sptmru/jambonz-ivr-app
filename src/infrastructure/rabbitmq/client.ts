e559e34db3467b6ba4d782b72d54f5605a7b5bdc
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-17 21:18:24 +0400

    increase consuming time interval in MQ client

--- src/infrastructure/rabbitmq/client.ts
+++ src/infrastructure/rabbitmq/client.ts
@@ -18,7 +18,7 @@ export class MQClient {
 
   private messageCount: number = 0;
   private readonly MAX_MESSAGES: number = config.rabbitmq.prefetchCount;
-  private readonly TIME_INTERVAL: number = 1000; // 1 second
+  private readonly TIME_INTERVAL: number = 10000;
 
   constructor() {
     this.connect();
