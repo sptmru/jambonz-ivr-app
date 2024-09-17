dbf40f61b697cfbc9d46f66f616b20db8ecbf1c6
Soslan Aldatov <soslanaldatov@gmail.com>
2024-09-17 16:15:29 +0400

    comment out WS server and MQ client for testing

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,33 @@
-import * as Sentry from '@sentry/node';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
-
 import { CallDetailsDefinition, SipAuthDataDefinition } from './domain/definitions/calldetails.definition';
 import { Api } from './infrastructure/api/server';
-import { config } from './infrastructure/config/config';
+// import { config } from './infrastructure/config/config';
+// import { MQClient } from './infrastructure/rabbitmq/client';
 import { CallbacksRoute } from './routes/callback/callback.route';
 import { CallsRoute } from './routes/calls/calls.route';
 import { HealthRoute } from './routes/health/health.route';
-
-Sentry.init({
-  dsn: config.sentry.dsn,
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: config.sentry.tracesSampleRate,
-  profilesSampleRate: config.sentry.profilesSampleRate,
-});
+import {TestRoute} from "./routes/test/test.route";
+// import { CallsService } from './services/calls/calls.service';
+// import { WebsocketServer } from './infrastructure/ws/server';
+// import { WsIvrEndpoint } from './routes/ws/ivr.endpoint';
 
 const api = new Api({
   plugins: [],
-  routes: [HealthRoute, CallsRoute, CallbacksRoute],
+  routes: [HealthRoute, CallsRoute, CallbacksRoute, TestRoute],
   definitions: [CallDetailsDefinition, SipAuthDataDefinition],
 });
 api.listen();
+
+// const ws = new WebsocketServer();
+// ws.addEndpoints([WsIvrEndpoint]);
+// ws.listen();
+//
+// const mq = MQClient.getInstance();
+// // eslint-disable-next-line require-await
+// mq.consumeToQueue(config.rabbitmq.callsQueue, CallsService.createCall);
+//
+// const onShutdown = (): void => {
+//   void mq.onShutdown();
+// };
+//
+// process.on('SIGINT', onShutdown);
+// process.on('SIGTERM', onShutdown);
