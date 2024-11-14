15f15a53a3643be0b798d768fd2c044c2d8fa599
Soslan Aldatov <soslanaldatov@gmail.com>
2024-11-14 12:55:54 +0400

    add caching for FS status API total calls request

--- src/services/third-party/fs-status-api-wrapper.service.ts
+++ src/services/third-party/fs-status-api-wrapper.service.ts
@@ -3,17 +3,30 @@ import { config } from "../../infrastructure/config/config";
 import { logger } from "../../misc/Logger";
 
 export class FSStatusApiWrapper {
-  public static baseUrl  = config.thirdParty.fsStatusApi.baseUrl;
+  private static baseUrl  = config.thirdParty.fsStatusApi.baseUrl;
+
+  private static cachedResult: boolean | null = null;
+  private static lastFetchedTime: number = 0;
+  private static cacheDuration: number = config.thirdParty.fsStatusApi.cacheTTL;
 
   static async checkIfWeCanProcessNewCalls(): Promise<boolean> {
+    const currentTime = Date.now();
+    if (this.cacheDuration !== 0 
+        && this.cachedResult !== null 
+        && currentTime - this.lastFetchedTime < this.cacheDuration) {
+      return this.cachedResult;
+    }
+
     try {
       const response = await axios.get(`${FSStatusApiWrapper.baseUrl}/instanceCalls/totalcalls`, {
         headers: { 
           Authorization: `Bearer ${config.thirdParty.fsStatusApi.bearerToken}`,
         },
       });
-      logger.info(`Number of calls from FSStatus API: ${response.data.totalCalls}`);
-      return response.data.totalCalls < config.calls.expectedNumberOfCalls;
+      this.cachedResult = response.data.totalCalls < config.calls.expectedNumberOfCalls;
+      this.lastFetchedTime = currentTime;
+
+      return this.cachedResult;
     } catch (err) {
       logger.error(`Error while getting number of calls from FSStatus API: ${err.message}`);
       return false;
