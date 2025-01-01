c3dbea1f46255b83de13e4e89e787e876cef7ffa
Soslan Aldatov <soslanaldatov@gmail.com>
2025-01-02 01:17:55 +0400

    add enum for call_status possible values, add a type for final call status values

--- src/domain/types/callstatus.type.ts
+++ src/domain/types/callstatus.type.ts
@@ -1,12 +1,33 @@
 import { CallDetails } from './calldetails.type';
 
+// eslint-disable-next-line no-shadow
+export enum CallStatusEnum {
+  TRYING = 'trying',
+  RINGING = 'ringing',
+  ALERTING = 'alerting',
+  IN_PROGRESS = 'in-progress',
+  COMPLETED = 'completed',
+  BUSY = 'busy',
+  NO_ANSWER = 'no-answer',
+  FAILED = 'failed',
+  QUEUED = 'queued',
+}
+
+export type FinalCallStatus = CallStatusEnum.COMPLETED | CallStatusEnum.BUSY | CallStatusEnum.NO_ANSWER | CallStatusEnum.FAILED;
+
+export const isFinalCallStatus = (value: string): value is FinalCallStatus =>
+  value === CallStatusEnum.COMPLETED ||
+  value === CallStatusEnum.BUSY ||
+  value === CallStatusEnum.NO_ANSWER ||
+  value === CallStatusEnum.FAILED;
+
 export type CallStatus = {
   account_sid: string;
   api_base_url: string;
   application_sid: string;
   call_id: string;
   call_sid: string;
-  call_status: string;
+  call_status: CallStatusEnum;
   call_termination_by: string;
   direction: string;
   duration: number;
