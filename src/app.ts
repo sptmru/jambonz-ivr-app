b65ddb903b0572e7e54b19b76f9c23fa58730832
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-19 12:32:59 +0400

    initialize Kubernetes Metrics API wrapper to refresh IVR app pod count over time

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ import { HealthRoute } from './routes/health/health.route';
 import { CallsService } from './services/calls/calls.service';
 import { WebsocketServer } from './infrastructure/ws/server';
 import { WsIvrEndpoint } from './routes/ws/ivr.endpoint';
+import { KubernetesMetricsApiWrapper } from './services/third-party/k8s-metrics-api-wrapper.service';
+
+KubernetesMetricsApiWrapper.getInstance(); // initializing k8s metrics API wrapper to start refreshing pod count automatically over time
 
 const api = new Api({
   plugins: [],
