312d8a9926718fa49b59ea7899ba0d6485570c4a
Soslan Aldatov <soslanaldatov@gmail.com>
2025-02-19 12:57:18 +0400

    add a route for stopping call handler from the outside of the app

--- src/routes/calls/calls.route-options.ts
+++ src/routes/calls/calls.route-options.ts
@@ -1,6 +1,55 @@
 import { Api } from '../../infrastructure/api/server';
 import { RouteOptionsWithoutHandler } from '../../infrastructure/api/types/RouteOptionsWithoutHandler';
 
+export const resolveCallHandlerRouteOptions: RouteOptionsWithoutHandler = {
+  method: 'POST',
+  url: '/api/v1/call/resolve/:callSid',
+  preValidation: [Api.addAuthToRoute],
+  schema: {
+    description:
+      'Accepts a call SID and resolves (ends) the call request handler function. Must be executed once a call is finished',
+    summary: 'Resolve call request handler function via HTTP API',
+    tags: ['calls'],
+    params: {
+      type: 'object',
+      properties: {
+        callSid: {
+          type: 'string',
+          description: 'Call SID',
+        },
+      },
+    },
+    security: [
+      {
+        ApiKeyAuth: [],
+      },
+    ],
+    response: {
+      200: {
+        description: 'Call resolved successfully',
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+        },
+      },
+      401: {
+        description: 'Authorization error',
+        type: 'object',
+        properties: {
+          message: { error: 'string' },
+        },
+      },
+      500: {
+        description: 'Error when trying to resolve a call',
+        type: 'object',
+        properties: {
+          error: { type: 'string' },
+        },
+      },
+    },
+  },
+};
+
 export const createCallRouteOptions: RouteOptionsWithoutHandler = {
   method: 'POST',
   url: '/api/v1/call',
